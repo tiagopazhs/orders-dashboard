899ad3fec27dab7f8fbd39fb383838d35784c78d
tiagopazhs <tiagopaz.hs@gmail.com>
2023-02-23 21:29:47 -0300

    change the react-router-dom

--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from './screens/Home';
-import Printer from './screens/Printer';
-import Reader from './screens/Reader';
-import ThreeRopes from './screens/ThreeRopes';
 import Dash from './screens/Dash';
 
 function App () {
@@ -12,10 +8,6 @@ function App () {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Dash />}  />
-        <Route path="/Printer-Customizer" element={<Printer />}/>
-        <Route path="/Three-Ropes" element={<ThreeRopes />}/>
-        <Route path="/Validation-Order" element={<Reader />}/>
-        <Route path="/home" element={<Home />}/>
       </Routes>
     </BrowserRouter>
   )
